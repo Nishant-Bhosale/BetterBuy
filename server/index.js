e9b986bef5d481b2101115ffabafce37505050c2
Nishant-Bhosale <nishantbhosale244@gmail.com>
2022-12-28 12:22:34 +0530

    Made changes

--- server/index.js
+++ server/index.js
@@ -17,10 +17,10 @@ app.use("/api/auth", authRouter);
 app.use("/api/product", productRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client/build"));
+  app.use(express.static("./build"));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, ".", "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, ".", "build", "index.html"))
   );
 }
 
