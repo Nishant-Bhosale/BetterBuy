c7f1852d2a9a2670b037e60de10096af0f3153c6
Nishant-Bhosale <nishantbhosale244@gmail.com>
2022-12-28 11:22:38 +0530

    Ready for deployment

--- server/index.js
+++ server/index.js
@@ -15,14 +15,14 @@ connectDB();
 app.use("/api/auth", authRouter);
 app.use("/api/product", productRouter);
 
-if (process.env.NODE_ENV === "production") {
-  //*Set static folder up in production
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   //*Set static folder up in production
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   );
+// }
 
 const PORT = process.env.PORT || 5000;
 
