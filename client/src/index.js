f276857400119a6151b9c4f6a246b3b4f1f6e346
Nishant-Bhosale <nishantbhosale244@gmail.com>
2022-12-28 12:41:49 +0530

    Made changes

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
